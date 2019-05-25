c73b2fa55db0514ef44cc40f83a5d3548f914211
stefian <v_ianta@yahoo.com>
2019-05-25 17:49:55 -0400

    added deactivate var in module to allow switching of active fam branches/paths

--- src/FamilyNode.svelte
+++ src/FamilyNode.svelte
@@ -1,21 +1,40 @@
 <script context="module">
   // Put here code that needs to run just once per module / file
   console.log("Runs once!");
+  let deactivateNode;
 </script>
 
 <script>
   export let member;
 
+  let isActive;
+
   console.log("Runs multiple times!");
+
+  function deactivate() {
+    isActive = false;
+  }
+
+  function activate() {
+    if (deactivateNode) {
+      deactivateNode();
+    }
+    isActive = true;
+    deactivateNode = deactivate;
+  }
 </script>
 
 <style>
   div {
     margin-left: 2rem;
   }
+
+  .active {
+    color: red;
+  }
 </style>
 
-<div>
+<div on:click={activate} class:active={isActive}>
   <h1>{member.name}</h1>
   {#if member.isParent}
     {#each member.children as child}
